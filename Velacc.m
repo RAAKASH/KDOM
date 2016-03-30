32bd9d908d62db771c36802c9927fcced495280b
AAKASH R <munna_akash@yahoo.co.in>
2016-03-30 12:38:27 +0530

    Update Velacc.m

--- Velacc.m
+++ Velacc.m
@@ -26,13 +26,11 @@ t = t/180*pi;   % conversion from degrees to radians
 
 A = [sin(t(:,3))*l(3),-sin(t(:,4))*l(4)];
 B = [cos(t(:,3))*l(3),-cos(t(:,4))*l(4)];
-% Q = [l(2)*cos(t(:,2))*(w(2))^2+l(3)*cos(t(:,3)).*((W(:,1)).^2) -l(4)*cos(t(:,4)).*((W(:,2)).^2) ,-l(2)*sin(t(:,2))*(w(2))^2-l(3)*sin(t(:,3)).*((W(:,1)).^2) +l(4)*sin(t(:,4)).*((W(:,2)).^2)];
-% Q = -1*Q
-% pause;
+
 a = zeros(length(t),2);
 for i=1:length(t)
       Q = [(l(2)*cos(t(i,2))*(w(2))^2 + l(3)*cos(t(i,3))*((W(i,1))^2)  -1* l(4)*cos(t(i,4))*((W(i,2)).^2)),(-1*l(2)*sin(t(i,2))*(w(2))^2-1*l(3)*sin(t(i,3))*((W(i,1))^2) +l(4)*sin(t(i,4))*((W(i,2))^2))]
-%       pause;
+      Q = -Q;
        
       if(norm([A(i,:);B(i,:)])~=0)
                   a(i,:) =   (([A(i,:);B(i,:)])\[Q(1);Q(2)])'; 
